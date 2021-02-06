be3bce6bfa13958ce1e590aa58cf7228a2364caa
Michael Iodice <miodice3@gmail.com>
2021-02-06 11:24:49 -0700

    commit tree 45f49cbb38379e6e7506b9e1acd100519dee7f03

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,66 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        };
+    }
+
+    handleClick = () => {
+        this.setState({
+            settings:{
+            ...this.state.settings,
+            bitrate: 12
+        }
+        }
+        )
+    };
+
+    handleClickRes = () => {
+        this.setState({
+            settings:{
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.resolution,
+                    resolution: '720p'
+                }
+            }
+
+        })
+    }
+
+    // handleClickRes = () => {
+    //     this.setState({
+    //         settings:{
+    //             video:{
+    //                 ...this.state.settings.video,
+    //                 resolution: '720p'
+    //             }
+    //         }
+    //     })
+    // }
+
+
+
+    render(){
+        return(
+            <div>
+            <button className="bitrate" onClick={this.handleClick}>{this.state.settings.bitrate}</button>
+            <button className="resolution" onClick={this.handleClickRes}>{this.state.settings.video.resolution}</button>
+            </div>
+            )
+    }
+
+}
